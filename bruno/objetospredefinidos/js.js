0eff9cad9a7222379a30cb8ff1f72f400c2e0ed5
Hector Santos <hecsanroq@gmail.com>
2018-11-05 18:39:59 +0100

    jquery2

--- bruno/objetospredefinidos/js.js
+++ bruno/objetospredefinidos/js.js
@@ -1,4 +1,29 @@
 $(document).ready(function(){
+   
+    boton.onclick=function(e){
+       
+
+        if(text01.value==""||text02.value==""){
+           
+            $("#text01").after("Rellena los dos campos.");
+            $("#text02").after("Rellena los dos campos.");
+
+        }
+        
+    }
+
+   /*
+    $("form")
+    .hide()
+    .fadeIn(10000);
+
+    $("text01")
+    .fadeOut(5000)
+    .fadeIn(5000);
+
+    var mensaje = document.getElementById("text01");
+    var mensaje =$("#text01")
+    */
 /*
 .html (inserta texto) 
     cambiandolo
