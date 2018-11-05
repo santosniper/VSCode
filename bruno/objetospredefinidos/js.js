1e14565f06eb85036894b8e9c1c59da2099f4a3c
Hector Santos <hecsanroq@gmail.com>
2018-11-05 17:17:47 +0100

    jquery00

---
+++ bruno/objetospredefinidos/js.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+/*
+.html (inserta texto) 
+    cambiandolo
+.text (inserta texto)
+.append (inserta texto)
+    detrás
+.prepend (inserta texto)
+    delante
+.before (inserta texto)
+    del elemnto
+.after (inserta después)
+    del elemento
+*/
+});
