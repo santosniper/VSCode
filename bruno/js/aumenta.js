5d529fc520e828140f97fc234d231612669f1621
Hector Santos <hecsanroq@gmail.com>
2018-11-15 20:12:10 +0100

    bruno aumento

---
+++ bruno/js/aumenta.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+
+
+    document.getElementById("aum1").addEventListener("click", func11, false);
+    document.getElementById("dis1").addEventListener("click", func12, false);;
+    document.getElementById("aum2").addEventListener("click", func21, false);;
+    document.getElementById("dis2").addEventListener("click", func22, false);;
+    document.getElementById("aum3").addEventListener("click", func31, false);;
+    document.getElementById("dis3").addEventListener("click", func32, false);;
+
+});
+
+function func11() {
+
+}
+
+function func12() {
+
+}
+
+function func21() {
+
+}
+
+function func22() {
+
+}
+
+function func31() {
+
+}
+
+function func32() {
+
+}
+
+
+
--- bruno/js/banner.js
+++ bruno/js/banner.js
@@ -2,25 +2,25 @@ var boton1 = document.getElementById("id_boton1");
 var boton2 = document.getElementById("id_boton2");
 var boton3 = document.getElementById("foto1");
 
-var resultado1= document.getElementById("msg1");
+var resultado1 = document.getElementById("msg1");
 var resultado2 = document.getElementById("msg2");
 
 
 
-boton1.onclick = function(e){
-    resultado1.value=resultado1.value+"Un click ";
+boton1.onclick = function (e) {
+    resultado1.value = resultado1.value + "Un click ";
     //resultado.value="1";
 }
 
-boton2.ondblclick = function(e){
-    resultado2.value=resultado2.value+"doble click ";
+boton2.ondblclick = function (e) {
+    resultado2.value = resultado2.value + "doble click ";
     //resultado.value="1";
 }
 
-boton3.onmouseover = function(e){
+boton3.onmouseover = function (e) {
     alert("La imagen no se toca!");
 }
-boton3.onmouseleave = function(e){
+boton3.onmouseleave = function (e) {
     alert("Gracias por tu colaboración");
 }
 
@@ -29,45 +29,45 @@ var boton = document.getElementById("cruz");
 
 boton.addEventListener("click", cerradv, false);
 
-function cerradv(){
-    
+function cerradv() {
+
     $("#superpuesto").remove();
 
 }
 
 im1.addEventListener("click", sold1, false);
 
-function sold1(){
-    
+function sold1() {
+
     $("#im1").replaceWith("<img src='img/965334.png'>");
 }
 
 im2.addEventListener("click", sold2, false);
 
-function sold2(){
-    
+function sold2() {
+
     $("#im2").replaceWith("<img src='img/965334.png'>");
 }
 
 im3.addEventListener("click", sold3, false);
 
-function sold3(){
-    
+function sold3() {
+
     $("#im3").replaceWith("<img src='img/965334.png'>");
 }
 
 im4.addEventListener("click", sold4, false);
 
-function sold4(){
-    
+function sold4() {
+
     $("#im").replaceWith("<img src='img/965334.png'>");
 }
 
 var botgoogle = document.getElementById("botgoogle");
 
 botgoogle.addEventListener("click", destacar1, false);
-function destacar1(){
+function destacar1() {
     $(".guguel").addClass("destacada");
-    
-   
+
+
 }
